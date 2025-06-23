068f516703b812e95ceaddf7ddec326eca52c0cb
Ygor Volstein <ygor.volstein@unifebe.edu.br>
2025-06-23 19:34:50 -0300

    Funções de liberar botão

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let pontosAdicionar = 1;
-const custoPrimeiroUpgrade = 100;
-const custoSegundoUpgrade = 500;
+const custoPrimeiroUpgrade = 50;
+const custoSegundoUpgrade = 100;
 const custoTerceiroUpgrade = 1000;
 
 const valorPrimeiroUpgrade = 1;
@@ -19,13 +19,13 @@ function adicionarPontos(){
     let numero = parseInt(pontos) + pontosAdicionar;
 
     if(numero >= custoPrimeiroUpgrade){
-        liberarPrimeiroUpgrade();
+        liberarUpgrade('comprar1');
     }
     if(numero >= custoSegundoUpgrade){
-        liberarSegundoUpgrade();
+        liberarUpgrade('comprar2');
     }
     if(numero >= custoTerceiroUpgrade){
-        liberarTerceiroUpgrade();
+        liberarUpgrade('comprar3');
     }
 
     elemento.innerText = `${numero}`;
@@ -35,19 +35,32 @@ function subtrairPontos(quantidade){
     let elemento = document.getElementById('pontos')
     let pontos = elemento.getHTML();
     let numero = parseInt(pontos) - quantidade;
+
+    if(numero < custoPrimeiroUpgrade){
+        bloquearUpgrade('comprar1');
+    }
+
+    if(numero < custoSegundoUpgrade){
+        bloquearUpgrade('comprar2');
+    }
+
+    if(numero < custoTerceiroUpgrade){
+        bloquearUpgrade('comprar3');
+    }
+
     elemento.innerText = `${numero}`;
 }
 
-function liberarPrimeiroUpgrade(){
-
+function liberarUpgrade(botao){
+    let elemento = document.getElementById(botao)
+    elemento.classList.remove('bloqueado');
+    elemento.classList.add('comprar');
 }
 
-function liberarSegundoUpgrade(){
-
-}
-
-function liberarTerceiroUpgrade(){
-
+function bloquearUpgrade(botao){
+    let elemento = document.getElementById(botao)
+    elemento.classList.remove('comprar');
+    elemento.classList.add('bloqueado');
 }
 
 function notificarPontosInsuficientes(){
@@ -71,6 +84,8 @@ function comprarPrimeiroUpgrade(){
     if(verificarPontosSuficientes(custoPrimeiroUpgrade)){
         pontosAdicionar += valorPrimeiroUpgrade;
         subtrairPontos(custoPrimeiroUpgrade);
+        aumentarPoderClique(valorPrimeiroUpgrade);
+        aumentarQuantidadeUpgrades();
     }
 }
 
@@ -78,12 +93,32 @@ function comprarSegundoUpgrade(){
     if(verificarPontosSuficientes(custoSegundoUpgrade)) {
         pontosAdicionar += valorSegundoUpgrade;
         subtrairPontos(custoSegundoUpgrade);
+        aumentarPoderClique(valorSegundoUpgrade);
+        aumentarQuantidadeUpgrades();
     }
 }
 
 function comprarTerceiroUpgrade(){
     if(verificarPontosSuficientes(custoTerceiroUpgrade)) {
         pontosAdicionar += valorTerceiroUpgrade;
-        subtrairPontos(custoTerceiroUpgrade)
+        subtrairPontos(custoTerceiroUpgrade);
+        aumentarPoderClique(valorTerceiroUpgrade);
+        aumentarQuantidadeUpgrades();
     }
+}
+
+function aumentarPoderClique(quantidade){
+    let elemento = document.getElementById('poder_clique')
+    let poder = elemento.getHTML();
+    let numero = parseInt(poder) + quantidade;
+
+    elemento.innerText = `${numero}`;
+}
+
+function aumentarQuantidadeUpgrades(){
+    let elemento = document.getElementById('qtd_upgrade')
+    let quantidade = elemento.getHTML();
+    let numero = parseInt(quantidade) + 1;
+
+    elemento.innerText = `${numero}`;
 }
